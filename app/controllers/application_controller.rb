fdf1d0579f6759d6fca0111f2f4a5816f4735e79
miki081 <tgjmtp.kkmd125@gmail.com>
2021-05-29 15:15:12 +0000

    5/30更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email])
+  end
+
+  protected
+  
+  def after_sign_in_path_for(resource)
+    new_customer_session_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+    new_customer_session_path(resource)
+  end
+  
+  def after_sign_out_path_for(resource)
+     homes_top_path
+  end
+
+   private
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,10 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable, :authentication_keys => [:login]
+         
+ 
+         
+  attr_accessor :login
+  
 end
