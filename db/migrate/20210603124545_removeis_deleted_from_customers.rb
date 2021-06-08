a36b5ac28e8ec524ea5e51760c10c5bc197fe14d
miki081 <tgjmtp.kkmd125@gmail.com>
2021-06-08 14:05:09 +0000

    6/8更新

---
+++ db/migrate/20210603124545_removeis_deleted_from_customers.rb
@@ -0,0 +1,4 @@
+class RemoveisDeletedFromCustomers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_134514) do
+ActiveRecord::Schema.define(version: 2021_06_05_022657) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_05_29_134514) do
     t.string "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "encrypted_password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -39,10 +46,13 @@ ActiveRecord::Schema.define(version: 2021_05_29_134514) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "authenticate_customers_at"
+    t.boolean "is_valid", default: true, null: false
+    t.boolean "admin"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_customers_on_deleted_at"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -85,6 +95,18 @@ ActiveRecord::Schema.define(version: 2021_05_29_134514) do
     t.integer "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
